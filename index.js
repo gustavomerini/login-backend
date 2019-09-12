4d3d8d31deb5f33d341d7e2fef9bab734dd958a8
merini <gutomerini@gmail.com>
2019-09-12 18:37:18 -0300

    Rate limiter

--- index.js
+++ index.js
@@ -2,7 +2,20 @@ const express = require('express')
 const path = require('path')
 const cors = require('cors')
 const app = express()
+const rateLimit = require('express-rate-limit')
 
+// Enable if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
+// see https://expressjs.com/en/guide/behind-proxies.html
+// app.set('trust proxy', 1);
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: 'Xiru veio ta tentando hackear, foi bloqueado biri biri'
+})
+
+//  apply to all requests
+app.use(limiter)
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'hbs')
 
